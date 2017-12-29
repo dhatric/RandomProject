51948ab7e5933aed73e555d24c0a657ed335c72e
gdhatric <gdhatric@deloitte.com>
2017-12-29 17:33:15 +0530

    languages Added

--- com/doodles/video/Main.py
+++ com/doodles/video/Main.py
@@ -1,15 +1,17 @@
 import DoodleExtractor
+import VideoDoodle
 
 if __name__ == '__main__':
     doodleObject = DoodleExtractor.getDoodleFromGoogle()
-    print doodleObject.get_doodle_title()
-    print doodleObject.get_doodle_name()
-    print doodleObject.get_doodle_query()
-    print doodleObject.get_doodle_image_png()
-    print doodleObject.get_doodle_image_jpeg()
-    for dooldleLang in doodleObject.get_doodle_dooleLangs():
-         print dooldleLang.get_doodle_lang()
-         print dooldleLang.get_doodle_query()
-         print dooldleLang.get_doodle_hoverText()
+    VideoDoodle.createDoodleVideo(doodleObject)
+    #print doodleObject.get_doodle_title()
+    #print doodleObject.get_doodle_name()
+    #print doodleObject.get_doodle_query()
+    #print doodleObject.get_doodle_image_png()
+    #print doodleObject.get_doodle_image_jpeg()
+    #for dooldleLang in doodleObject.get_doodle_dooleLangs():
+         #print dooldleLang.get_doodle_lang()
+         #print dooldleLang.get_doodle_query()
+         #print dooldleLang.get_doodle_hoverText()
 
         
---
+++ com/doodles/video/VideoDoodle.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*- 
+
+
+from Doodle import Doodle
+from DoodleLang import DoodleLang
+import sys
+from moviepy.editor import *
+
+width=1400
+height=width*9/16
+screensize = (width,height)
+wordWidth=width-40
+wordHeight=height/5
+
+output_video_directory='../output/videos/'
+
+def createDoodleVideo(doodleObject):
+    textCollection=[]
+    reload(sys)  # Reload does the trick!
+    sys.setdefaultencoding('UTF8')
+    print "creating video for "+doodleObject.get_doodle_title()
+    for dooldleLang in doodleObject.get_doodle_dooleLangs():
+        if dooldleLang.get_doodle_hoverText() is not None:      
+            txt_word_header = TextClip("Google Doodle ",color='white',font='arial',method='label',size=(wordWidth,50))
+            txt_word_header = txt_word_header.set_pos(('center',100)).set_duration(5)
+            textCollection.append(txt_word_header)
+            background_clip = ImageClip(doodleObject.get_doodle_image_jpeg())
+            background_clip = background_clip.set_pos(('center','center')).set_duration(5)
+            textCollection.append(background_clip)
+            txt_word = TextClip(dooldleLang.get_doodle_hoverText(),color='white',font='arial',method='label',size=(wordWidth,wordHeight))
+            txt_word = txt_word.set_pos(('center',600)).set_duration(5)
+            textCollection.append(txt_word)
+            video = CompositeVideoClip(textCollection,size=screensize)
+            absoluteVideoFile=output_video_directory+doodleObject.get_doodle_name()[:20]+"_"+dooldleLang.get_doodle_lang()+".mp4"
+            video.write_videofile(absoluteVideoFile,fps=2,codec="mpeg4") 
+
+    
+    
+    
+    
