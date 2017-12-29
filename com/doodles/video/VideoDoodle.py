bcaeca30b52c9c6a4f858d36286be73ec1587122
gdhatric <gdhatric@deloitte.com>
2017-12-29 18:49:38 +0530

    With zzom in effect

--- com/doodles/video/VideoDoodle.py
+++ com/doodles/video/VideoDoodle.py
@@ -14,6 +14,16 @@ wordHeight=height/5
 
 output_video_directory='../output/videos/'
 
+
+def resize_func(t):
+    if t < 4:
+        return 1 + 0.08*t  # Zoom-in.
+    elif 4 <= t <= 6:
+        return 1 + 0.08*4  # Stay.
+    else: # 6 < t
+        return 1 + 0.08*(10-t)  # Zoom-out.
+    
+
 def createDoodleVideo(doodleObject):
     reload(sys)  # Reload does the trick!
     sys.setdefaultencoding('UTF8')
@@ -23,17 +33,17 @@ def createDoodleVideo(doodleObject):
             textCollection=[]    
             print dooldleLang.get_doodle_hoverText()
             txt_word_header = TextClip("Google Doodle ",color='white',font='arial',method='label',size=(wordWidth,50))
-            txt_word_header = txt_word_header.set_pos(('center',62)).set_duration(5)
+            txt_word_header = txt_word_header.set_pos(('center',62)).set_duration(10)
             textCollection.append(txt_word_header)
             background_clip = ImageClip(doodleObject.get_doodle_image_jpeg())
-            background_clip = background_clip.set_pos(('center',114)).set_duration(5)
+            background_clip = background_clip.resize(resize_func).set_pos(('center',114)).set_duration(10)
             textCollection.append(background_clip)
             txt_word = TextClip(dooldleLang.get_doodle_hoverText(),color='white',font='Arial-Unicode-MS',method='caption',size=(wordWidth,wordHeight),print_cmd=True)
-            txt_word = txt_word.set_pos(('center',570)).set_duration(5)
+            txt_word = txt_word.set_pos(('center',570)).set_duration(10)
             textCollection.append(txt_word)
             video = CompositeVideoClip(textCollection,size=screensize)
             absoluteVideoFile=output_video_directory+doodleObject.get_doodle_name()[:20]+"_"+dooldleLang.get_doodle_lang()+".mp4"
-            video.write_videofile(absoluteVideoFile,fps=2,codec="mpeg4") 
+            video.write_videofile(absoluteVideoFile,fps=24,codec="mpeg4") 
 
     
     
