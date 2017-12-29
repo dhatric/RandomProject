61949c8680e5c900c736b52f6692a6879e7db531
gdhatric <gdhatric@deloitte.com>
2017-12-29 18:22:01 +0530

    Fixed langauage unicode issue

--- com/doodles/video/VideoDoodle.py
+++ com/doodles/video/VideoDoodle.py
@@ -20,14 +20,15 @@ def createDoodleVideo(doodleObject):
     sys.setdefaultencoding('UTF8')
     print "creating video for "+doodleObject.get_doodle_title()
     for dooldleLang in doodleObject.get_doodle_dooleLangs():
-        if dooldleLang.get_doodle_hoverText() is not None:      
+        #if dooldleLang.get_doodle_hoverText() is not None and dooldleLang.get_doodle_lang()=='hi':      
+            print dooldleLang.get_doodle_hoverText()
             txt_word_header = TextClip("Google Doodle ",color='white',font='arial',method='label',size=(wordWidth,50))
             txt_word_header = txt_word_header.set_pos(('center',100)).set_duration(5)
             textCollection.append(txt_word_header)
             background_clip = ImageClip(doodleObject.get_doodle_image_jpeg())
             background_clip = background_clip.set_pos(('center','center')).set_duration(5)
             textCollection.append(background_clip)
-            txt_word = TextClip(dooldleLang.get_doodle_hoverText(),color='white',font='arial',method='label',size=(wordWidth,wordHeight))
+            txt_word = TextClip(dooldleLang.get_doodle_hoverText(),color='white',font='Arial-Unicode-MS',method='caption',size=(wordWidth,wordHeight),print_cmd=True)
             txt_word = txt_word.set_pos(('center',600)).set_duration(5)
             textCollection.append(txt_word)
             video = CompositeVideoClip(textCollection,size=screensize)
